6fd16450c758d07042ce0b322ed2bd42dee53bc7
Kevin Karma <kevinkarma55@gmail.com>
2019-08-12 22:51:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,12 @@
 # My Code here....
 def map_to_negativize(source_array) 
-  source_array.map {|i| i * -1}
+  source_array.map { |i| i * -1 }
 end
 
 def map_to_no_change(source_array)
-  source_array.map {|i| i}
+  source_array.map { |i| i }
+end
+
+def map_to_double(source_array)
+  source_array.map { |i| i *2 }
 end
