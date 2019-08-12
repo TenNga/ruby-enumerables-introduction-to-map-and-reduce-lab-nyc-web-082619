1745a63dfe7cd9741f00e3581992b8f0fd9bf836
Kevin Karma <kevinkarma55@gmail.com>
2019-08-12 23:45:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,10 @@ def map_to_square(source_array)
   source_array.map { |i| i * i }
 end
 
+def reduce_to_total(source_array)
+  source_array.reduce(:+)
+end
+
 def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point,:+)
 end
