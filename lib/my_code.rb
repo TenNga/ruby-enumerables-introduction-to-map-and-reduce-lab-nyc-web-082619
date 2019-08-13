471f467fe46b832c7da302e30f2675a2908713b1
Kevin Karma <kevinkarma55@gmail.com>
2019-08-13 00:03:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,9 +16,9 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array)
-  source_array.reduce(0) { |sum, n| sum +n }
+  source_array.inject(0) { |sum, n| sum +n }
 end
 
 def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point) { |sum, n| sum +n }
+  source_array.inject(starting_point) { |sum, n| sum +n }
 end
